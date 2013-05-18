4514a7b92f782bad28336fc0efc6ce00acc75944
Anastasis Germanidis <agermanidis@gmail.com>
2013-05-18 12:23:46 -0400

    Started working on the server

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search_by_name", methods = ['POST'])
+def search(name):
+    pass
+
+
+
+if __name__ == "__main__":
+    app.run()
+
